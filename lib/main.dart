b30a43695f7fe3476b82f5bcb48afc5eb096b251
mishalhaneef <msldroidofficial@gmail.com>
2023-03-06 22:24:23 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:coffee_application/view/coffee_list.dart';
+import 'package:coffee_application/view/coffee_home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
